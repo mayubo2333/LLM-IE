c0b7510887fc358b9b913b39ec66ed3031adb35e
Yubo Ma <yubo001@e.ntu.edu.sg>
2024-03-01 03:23:39 -0500

    add scripts

---
+++ scripts/run_re_tacrev.sh
@@ -0,0 +1,17 @@
+export SLM_RES_PATH="TO BE FILLED"
+export OUT_PATH="TO BE FILLED"
+
+python run_gpt4_reranker.py \
+    --dataset_name RE_TACREV \
+    --old_res_path $SLM_RES_PATH \
+    --demo_path ./prompt/demo_re_tacrev.json \
+    --output_path $OUT_PATH \
+    --model_name gpt-4 \
+    --batch_size 4 \
+    --temperature 0.0 \
+    --repeat_time 1 \
+    --topk 3 \
+    --demo_given \
+    --demo_num 4 \
+    --explanation_given \
+    --th_ub 0.7
